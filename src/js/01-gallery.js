b5f5e3c9edad3895690690fb47c1a54159e62a2b
JulitaZochowska <julita.zochowska@outlook.com>
2023-04-28 11:30:50 +0200

    Initial commit

---
+++ src/js/01-gallery.js
@@ -0,0 +1,7 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+console.log(SimpleLightbox);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+console.log(galleryItems);
