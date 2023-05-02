89ee8200ac389ea25ef478029f0f0ef29055b66d
JulitaZochowska <julita.zochowska@outlook.com>
2023-05-02 20:04:42 +0200

    Exercise 2 fixed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,41 @@
+//podpunkt 2
+import Player from '@vimeo/player';
+import _ from 'lodash';
 
+//podpunkt 3-done
+// Select with the DOM API
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+//podpunkt 7 - definicja
+const persistTimeThrottled = _.throttle(seconds => {
+  //podpunkt 5-done
+  localStorage.setItem('videoplayer-current-time', seconds);
+}, 1000);
+
+//podpunkt 4
+// timeupdate
+
+// Triggered as the currentTime of the video updates. It generally fires every 250ms, but it may vary depending on the browser.
+
+// {
+//     duration: 61.857
+//     percent: 0.049
+//     seconds: 3.034 <- tyle sekund filmu upłynęło
+// }
+player.on('timeupdate', data => {
+  // data is an object containing properties specific to that event
+  // podpunkt 7 - uzycie
+  persistTimeThrottled(data.seconds);
+});
+
+//podpunkt 6
+const timeFromLocalStorage = localStorage.getItem('videoplayer-current-time');
+player
+  .setCurrentTime(timeFromLocalStorage)
+  .then(function (seconds) {
+    console.log('Film załadowany na: ', seconds, ' sekundzie');
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,7 @@
-
+window.addEventListener('load', () => {
+  const form = document.querySelector('.feedback-form)');
+  const submitButton = document.querySelector('button');
+  const emailInput = document.querySelector('input');
+  const messageTextarea = document.querySelector('textarea');
+});
+function drawEntry(key) {}
