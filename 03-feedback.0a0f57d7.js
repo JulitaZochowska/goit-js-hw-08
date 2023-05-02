2a3823cd42ebeefa8576acccf3e27f85437a957a
JulitaZochowska <119934821+JulitaZochowska@users.noreply.github.com>
2023-05-02 18:05:11 +0000

    Deploying to gh-pages from @ JulitaZochowska/goit-js-hw-08@89ee8200ac389ea25ef478029f0f0ef29055b66d 🚀

---
+++ 03-feedback.0a0f57d7.js
@@ -0,0 +1,2 @@
+window.addEventListener("load",(()=>{document.querySelector(".feedback-form)"),document.querySelector("button"),document.querySelector("input"),document.querySelector("textarea")}));
+//# sourceMappingURL=03-feedback.0a0f57d7.js.map
---
+++ 03-feedback.61b61317.js
@@ -0,0 +1,2 @@
+window.addEventListener("load",(function(){document.querySelector(".feedback-form)"),document.querySelector("button"),document.querySelector("input"),document.querySelector("textarea")}));
+//# sourceMappingURL=03-feedback.61b61317.js.map
