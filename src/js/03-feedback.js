af8db5365613892005f317d53ba3b42951970fd5
JulitaZochowska <julita.zochowska@outlook.com>
2023-05-04 15:15:15 +0200

    Exercise 3-fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,56 @@
+import _ from 'lodash';
+const { entries } = require('lodash');
+//przeładowanie strony
+//podpunkt 2-done
 window.addEventListener('load', () => {
-  const form = document.querySelector('.feedback-form)');
-  const submitButton = document.querySelector('button');
+  const form = document.querySelector('.feedback-form');
   const emailInput = document.querySelector('input');
   const messageTextarea = document.querySelector('textarea');
+  const formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (formData) {
+    emailInput.value = formData.email;
+    messageTextarea.value = formData.message;
+  } else {
+    emailInput.value = '';
+    messageTextarea.value = '';
+  }
+
+  //podpunkt 4-done
+  const throttledUpdateStorage = _.throttle(value => {
+    localStorage.setItem('feedback-form-state', value);
+  }, 500);
+
+  //podpunkt 1-done
+  function saveFormDataToStorage() {
+    const formData = {
+      email: emailInput.value,
+      message: messageTextarea.value,
+    };
+
+    throttledUpdateStorage(JSON.stringify(formData));
+  }
+
+  //event input-zdarzenie, wywoływane za kazdym razem, gdy wartość pola formularza
+  //zostaje zmieniona przez uytkownika.
+  emailInput.addEventListener('input', saveFormDataToStorage);
+  messageTextarea.addEventListener('input', saveFormDataToStorage);
+
+  //podpunkt 3-done
+  form.addEventListener('submit', event => {
+    event.preventDefault(); //zatrzymujemy domyślne działanie formularza
+    localStorage.clear(); //usuwamy dane ze storage
+
+    //tworzymy obiekt z aktualnymi wartościami pól
+    const formData = {
+      email: emailInput.value,
+      message: messageTextarea.value,
+    };
+
+    //wyświetlamy obiekt z wartościami pól w konsoli
+    console.log(formData);
+
+    //czyścimy pola formularza dopiero po wypisaniu danych na konsolę
+    emailInput.value = '';
+    messageTextarea.value = '';
+  });
 });
-function drawEntry(key) {}
